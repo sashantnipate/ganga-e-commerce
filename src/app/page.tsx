30346e35eb48e666ebfcfaf9e09d7d110a718138
sashantnipate <sashantnipate@gmail.com>
2026-08-29 08:06:40 +0530

    feat(ui): add shadcn/ui with all components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      Hello world
+    <div className="flex justify-evenly items-center h-screen">
+      <Button>
+        Click me 
+      </Button>
     </div>
   );
 }
